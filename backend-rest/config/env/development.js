496261a6f70a272efb78f903565bf8bd8233cb3f
jescares <jescares@my.centennialcollege.ca>
2024-03-20 09:44:06 -0400

    updated env

--- backend-rest/config/env/development.js
+++ backend-rest/config/env/development.js
@@ -1,7 +1,7 @@
 //Development configuration options
 //To sign the session identifier, use a secret string
 module.exports = {
-  db: "mongodb+srv://jescares:jescares2024@skyhub-users.kcxdxbo.mongodb.net/?retryWrites=true&w=majority&appName=SkyHub-Users",
+  db: process.env.MONGODB_URI,
   sessionSecret: "developmentSessionSecret",
   secretKey: "real_secret",
 };
