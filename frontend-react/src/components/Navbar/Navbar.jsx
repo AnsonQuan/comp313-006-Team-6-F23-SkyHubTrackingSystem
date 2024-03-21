3cedd9878888a1ae38e821924e70a0f056f4029f
Muhammad Farhan Sohail <91357098+farhansohail1501@users.noreply.github.com>
2024-03-21 19:16:46 -0400

    Update Navbar.jsx

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,1 +1,35 @@
+import React, { useState } from 'react';
+import './Navbar.css'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; 
+import { Link } from 'react-router-dom'; 
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <Link to="/" className="logo">Skyhub</Link> 
+        <div className={`nav-links ${showMenu ? 'active' : ''}`}>
+          <Link to="/about">About</Link> 
+          <Link to="/contact">Contact</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/register">Register</Link>
+          <Link to="/review">Review</Link>
+          <Link to="/track">Track</Link>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FontAwesomeIcon icon={showMenu ? faTimes : faBars} /> 
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
 
