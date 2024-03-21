2f7af9d3326baee8fe352dded49d68173150fb81
Muhammad Farhan Sohail <91357098+farhansohail1501@users.noreply.github.com>
2024-03-21 19:13:36 -0400

    Update About.jsx

--- frontend-react/src/components/About/About.jsx
+++ frontend-react/src/components/About/About.jsx
@@ -1,1 +1,52 @@
+import React from 'react';
+import './About.css'; // Import the CSS file
+
+const About = () => {
+    return (
+        <div>
+        
+            <div className="about-us-container">
+                <h1>SkyHub</h1>
+                <p>
+                    At SkyHub, we are on a mission to revolutionize the travel business by offering a creative and user-friendly
+                    solution that goes beyond traditional reservation techniques. Our vision is to transform the travel industry
+                    by providing a cutting-edge platform that redefines the way we plan and experience air travel.
+                </p>
+
+                <h2>Our Mission</h2>
+                <p>
+                    Our mission is clear – to enhance the overall user experience by providing a feature-rich website that
+                    ensures quick and easy account creation, secure login credentials, and simplified booking processes for
+                    airline tickets, seats, and class types.
+                </p>
+
+                <h2>Key Objectives</h2>
+                <ul>
+                    <li>User Experience Enhancement</li>
+                    <li>Real-Time Flight Schedule Tracking</li>
+                    <li>Analytics and Reporting</li>
+                    <li>Cutting-Edge Technologies</li>
+                </ul>
+
+                <h2>Our Vision</h2>
+                <p>
+                    SkyHub aspires to be a pioneer in the travel industry, setting new standards for convenience, efficiency,
+                    and customer satisfaction. Through strategic partnerships with airlines and service providers, we aim to
+                    build a dependable, adaptable, and user-friendly platform that elevates the travel planning experience.
+                </p>
+
+                <h2>Join Us on the Journey</h2>
+                <p>
+                    SkyHub invites you to join us on this exciting journey to redefine travel. As we continue to grow and
+                    evolve, we promise to remain dedicated to providing a platform that meets and exceeds your expectations.
+                </p>
+
+        
+            </div>
+  
+        </div>
+    );
+};
+
+export default About;
 
