603b7d82dbe05a8c0db5e480965b0db7ae80e611
Muhammad Farhan Sohail <91357098+farhansohail1501@users.noreply.github.com>
2024-03-30 03:24:08 -0400

    Update About.jsx

--- frontend-react/src/components/About/About.jsx
+++ frontend-react/src/components/About/About.jsx
@@ -1,52 +1,61 @@
 import React from 'react';
-import './About.css'; // Import the CSS file
+import './About.css'; 
+import Card from './Card'; 
 
 const About = () => {
     return (
-        <div>
-        
-            <div className="about-us-container">
-                <h1>SkyHub</h1>
-                <p>
-                    At SkyHub, we are on a mission to revolutionize the travel business by offering a creative and user-friendly
+        <div className="about-page-container">
+            <div className="card-row">
+                <Card
+                    imageSrc="/images/About/SKYHUB.jpg"
+                    title="SkyHub"
+                    text=" At SkyHub, we are on a mission to revolutionize the travel business by offering a creative and user-friendly
                     solution that goes beyond traditional reservation techniques. Our vision is to transform the travel industry
-                    by providing a cutting-edge platform that redefines the way we plan and experience air travel.
-                </p>
-
-                <h2>Our Mission</h2>
-                <p>
-                    Our mission is clear – to enhance the overall user experience by providing a feature-rich website that
+                    by providing a cutting-edge platform that redefines the way we plan and experience air travel."
+                />
+                <Card
+                    imageSrc="/images/About/Our_Mission.jpg"
+                    title="Our Mission"
+                    text="Our mission is clear – to enhance the overall user experience by providing a feature-rich website that
                     ensures quick and easy account creation, secure login credentials, and simplified booking processes for
-                    airline tickets, seats, and class types.
-                </p>
-
-                <h2>Key Objectives</h2>
-                <ul>
-                    <li>User Experience Enhancement</li>
-                    <li>Real-Time Flight Schedule Tracking</li>
-                    <li>Analytics and Reporting</li>
-                    <li>Cutting-Edge Technologies</li>
-                </ul>
-
-                <h2>Our Vision</h2>
-                <p>
-                    SkyHub aspires to be a pioneer in the travel industry, setting new standards for convenience, efficiency,
-                    and customer satisfaction. Through strategic partnerships with airlines and service providers, we aim to
-                    build a dependable, adaptable, and user-friendly platform that elevates the travel planning experience.
-                </p>
-
-                <h2>Join Us on the Journey</h2>
-                <p>
-                    SkyHub invites you to join us on this exciting journey to redefine travel. As we continue to grow and
-                    evolve, we promise to remain dedicated to providing a platform that meets and exceeds your expectations.
-                </p>
-
-        
+                    airline tickets, seats, and class types."
+                />
             </div>
-  
+            <div className="card-row">
+            <Card
+    imageSrc="/images/About/Key_objectives.jpg"
+    title="Key Objectives"
+    text={
+        <>
+        
+            <ul>
+                <li>User Experience Enhancement</li>
+                <li>Real-Time Flight Schedule Tracking</li>
+                <li>Analytics and Reporting</li>
+                <li>Cutting-Edge Technologies</li>
+            </ul>
+        </>
+    }
+/>
+
+                <Card
+                    imageSrc="/images/About/Our_Vision.jpg"
+                    title="Our Vision"
+                    text=" SkyHub aspires to be a pioneer in the travel industry, setting new standards for convenience, efficiency,
+                    and customer satisfaction. Through strategic partnerships with airlines and service providers, we aim to
+                    build a dependable, adaptable, and user-friendly platform that elevates the travel planning experience."
+                />
+            </div>
+            <div className="card-row center-row">
+                <Card
+                    imageSrc="/images/About/Join_us.jpg"
+                    title="Join Us on the Journey"
+                    text="SkyHub invites you to join us on this exciting journey to redefine travel. As we continue to grow and
+                    evolve, we promise to remain dedicated to providing a platform that meets and exceeds your expectations."
+                />
+            </div>
         </div>
     );
 };
 
 export default About;
-
