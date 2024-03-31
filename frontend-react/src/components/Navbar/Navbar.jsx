1dd126eb6439c65264b5678fe9c51d67291a6bfc
Muhammad Farhan Sohail <91357098+farhansohail1501@users.noreply.github.com>
2024-03-31 17:14:43 -0400

    Update Navbar.jsx

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import './Navbar.css'; 
+import './Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faTimes, faHome, faSignInAlt, faUserPlus, faSearch, faStar, faPhone, faPlane, faInfoCircle } from '@fortawesome/free-solid-svg-icons'; 
-import { Link } from 'react-router-dom'; 
+import { faBars, faTimes, faHome, faSignInAlt, faUserPlus, faSearch, faStar, faPhone, faPlane, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -14,16 +14,19 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <Link to="/" className="logo">Skyhub</Link> 
+        <Link to="/" className="logo">
+          <img src="/images/About/SKYHUB.jpg" alt="Skyhub Logo" className="logo-img" />
+          <span className="logo-text">Skyhub</span>
+        </Link>
         <div className={`nav-links ${showMenu ? 'active' : ''}`}>
-          <Link to="/about"><FontAwesomeIcon icon={faInfoCircle} /> About</Link> 
+          <Link to="/about"><FontAwesomeIcon icon={faInfoCircle} /> About</Link>
           <Link to="/contact"><FontAwesomeIcon icon={faPhone} /> Contact</Link>
           <Link className="login-item" to="/login"><FontAwesomeIcon icon={faSignInAlt} /> Login</Link>
           <Link to="/review"><FontAwesomeIcon icon={faStar} /> Review</Link>
           <Link to="/track"><FontAwesomeIcon icon={faPlane} /> Track</Link>
         </div>
         <div className="menu-icon" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={showMenu ? faTimes : faBars} /> 
+          <FontAwesomeIcon icon={showMenu ? faTimes : faBars} />
         </div>
       </div>
     </nav>
